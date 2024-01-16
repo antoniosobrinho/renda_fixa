d89635d916895009436a3682d81e425934be6bf7
Antonio Mourão <antonio.smsobrinho@gmail.com>
2024-01-16 14:58:16 -0300

    change document

--- app/models/investment_simulation.py
+++ app/models/investment_simulation.py
@@ -12,11 +12,3 @@ class InvestmentSimulation(Document):
     monthly_investment = FloatField(required=True, min_value=0)
     monthly_interest_rate = FloatField(required=True, min_value=0)
     compensation = StringField(required=True)
-
-    def clean(self):
-        if self.compensation not in self.COMPENSATION_CHOICES:
-            raise ValidationError(f'Compensation must be one of these: {", ".join(self.COMPENSATION_CHOICES)}')
-        
-    def save(self, *args, **kwargs):
-        self.clean()
-        super().save(*args, **kwargs)
