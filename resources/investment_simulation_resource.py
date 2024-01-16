536bd1618217765f2d92916d49c41ece5e9eb3b8
Antonio Mourão <antonio.smsobrinho@gmail.com>
2024-01-16 09:41:50 -0300

    calculate final amount

--- resources/investment_simulation_resource.py
+++ resources/investment_simulation_resource.py
@@ -2,6 +2,8 @@ from flask_restful import Resource
 from flask import request
 from db.config import db
 from schemas.investment_simulation_schema import InvestmentSimulationSchema
+from utils.investment_utils import InvestmentUtils
+
 investment_simulation_collection = db['investment_simulations']
 
 class InvestmentSimulationResource(Resource):
@@ -18,10 +20,34 @@ class InvestmentSimulationResource(Resource):
         if errors:
             return {'error': 'Validation error', 'message': errors}, 400
 
+        initial_value = data['initial_value']
+        monthly_interest_rate = data['monthly_interest_rate']
+        months_invested = data['months_invested']
+        monthly_investment = data['monthly_investment']
+
+        #percent
+        monthly_interest_rate = monthly_interest_rate/100
         
+        investment_utils = InvestmentUtils()
+
+        final_amount = investment_utils.calculate_compound_interest(
+            initial_value, monthly_interest_rate, 
+            months_invested, monthly_investment
+        )
+        
+        total_invested = initial_value + monthly_investment * months_invested
+
+        total_interest = round((final_amount - total_invested),2)
+
+        response_data = {
+            "final_amount": final_amount,
+            "total_invested": total_invested,
+            "total_interest": total_interest
+        }
+
         # investment_simulation_id = investment_simulation_collection.insert_one(
         #                                                             {'data': json_data}
         #                                                         ).inserted_id
 
-        # return {str(investment_simulation_id): json_data}
+        return response_data
     
--- schemas/investment_simulation_schema.py
+++ schemas/investment_simulation_schema.py
@@ -2,8 +2,8 @@ from marshmallow import Schema, fields, validate
 
 class InvestmentSimulationSchema(Schema):
     name = fields.Str(required=True, validate=validate.Length(min=1))
-    period = fields.Int(required=True, validate=validate.Range(min=1))
-    initial_value = fields.Int(required=True, validate=validate.Range(min=0))
-    monthly_investment = fields.Int(required=True, validate=validate.Range(min=0))
+    months_invested = fields.Int(required=True, validate=validate.Range(min=1))
+    initial_value = fields.Float(required=True, validate=validate.Range(min=0))
+    monthly_investment = fields.Float(required=True, validate=validate.Range(min=0))
     monthly_interest_rate = fields.Float(required=True, validate=validate.Range(min=0))
     compensation = fields.Str(required=True, validate=validate.OneOf(['post-fixed', 'pre-fixed']))
