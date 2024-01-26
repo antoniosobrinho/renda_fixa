38b7fc0a8772e2152ccde670bff1108ea5fbe706
Antonio Mourão <antonio.smsobrinho@gmail.com>
2024-01-26 19:29:04 -0300

    swagger.yml

--- resources/investment_simulation_resource.py
+++ resources/investment_simulation_resource.py
@@ -1,66 +1,16 @@
 from flask_restful import Resource
 from flask import request, jsonify
 from marshmallow import ValidationError
+from flasgger import swag_from
 from schemas.investment_simulation_schema import InvestmentSimulationSchema
 from services.investment_simulation_service import InvestmentSimulationService
 from repositories.investment_simulation_repository import InvestmentSimulationRepository
 
 class InvestmentSimulationResource(Resource):
 
+    @swag_from('../swagger/swagger.yml')
     def post(self):
-        """
-        Post an investment simulation
-        ---
-        parameters:
-            -   name: name,
-                in: query
-                type: string
-                required: true
-                description: Name of the simulation
-            -   name: months_invested,
-                in: query
-                type: integer
-                required: true
-                description: Time in months in which the money will be invested
-            -   name: initial_value,
-                in: query
-                type: number
-                required: true
-                description: Initial value of the aplication
-            -   name: monthly_investment,
-                in: query
-                type: number
-                required: true
-                description: Amount invested each month
-            -   name: monthly_interest_rate,
-                in: query
-                type: number
-                required: true
-                description: Percentage earned each month
-            -   name: compensation,
-                in: query
-                type: string
-                enum: ["pre-fixed", "post-fixed"]
-                required: true
-                description: Name of the simulation
-        responses:
-            201:
-                description: Simulation created
-                schema:
-                    properties:
-                        final_amount:
-                            type: number
-                            description: Total value after months
-                        total_invested:
-                            type: number
-                            description: Total value invested after months
-                        total_interest:
-                            type: number
-                            description: Profit received
-            400:
-                description: Invalid request
-        """
-
+        
         data = request.get_json() 
         simulation_schema = InvestmentSimulationSchema()
         try:
--- tests/resources/test_investment_insights_resource.py
+++ tests/resources/test_investment_insights_resource.py
@@ -6,20 +6,20 @@ class TestInvestmentInsightsResource(SetUpTest):
     def setUpClass(cls):
         super().setUpClass()
 
-    #     for i in range(5):
-    #         cls.create_investment_simulation()
+        for i in range(5):
+            cls.create_investment_simulation()
 
-    # def test_get_investment_insights(self):
+    def test_get_investment_insights(self):
 
-    #     response = self.app.get('/investment_insights')
-    #     self.assertEqual(response.status_code, 200)
-    #     fields  = ['with_max_final_amount',
-    #                 'with_min_final_amount',
-    #                 'avg_final_amount',
-    #                 'with_max_monthly_investment',
-    #                 'with_min_monthly_investment']
+        response = self.app.get('/investment_insights')
+        self.assertEqual(response.status_code, 200)
+        fields  = ['with_max_final_amount',
+                    'with_min_final_amount',
+                    'avg_final_amount',
+                    'with_max_monthly_investment',
+                    'with_min_monthly_investment']
         
-    #     data = response.get_json()
-    #     data_keys = data.keys()
-    #     for field in fields:
-    #         self.assertIn(field, data_keys)
+        data = response.get_json()
+        data_keys = data.keys()
+        for field in fields:
+            self.assertIn(field, data_keys)
