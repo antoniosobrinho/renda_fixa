76cfdad6458ddf5447b5739b80f44fb4cdcdbf31
Antonio Mourão <antonio.smsobrinho@gmail.com>
2024-01-26 20:38:14 -0300

    refactor swagger

--- resources/investment_insights_resource.py
+++ resources/investment_insights_resource.py
@@ -1,37 +1,14 @@
 from flask_restful import Resource
-from services.investment_simulation_service import InvestmentSimulationService
+from flasgger import swag_from
+from services.investment_insights_service import InvestmentInsightsService
 from repositories.investment_simulation_repository import InvestmentSimulationRepository
 
 class InvestmentInsightsResource(Resource):
 
+    @swag_from('../swagger/investment_insights/get.yml')
     def get(self):
-        """
-        Get an investment insights
-        ---
-        responses:
-            200:
-                description: Investment insights
-                schema:
-                    properties:
-                        with_max_final_amount:
-                            type: number
-                            description: simulation with maximum final amount
-                        with_min_final_amount:
-                            type: number
-                            description: simulation with minimum final amount
-                        avg_final_amount:
-                            type: number
-                            description: avarege final amount of all simulations
-                        with_max_monthly_investment:
-                            type: number
-                            description: simulation with maximum monthly investment
-                        with_min_monthly_investment:
-                            type: number
-                            description: simulation with minimum monthly investment
-
-        """
-        
-        service = InvestmentSimulationService(
+               
+        service = InvestmentInsightsService(
             InvestmentSimulationRepository()
         )
 
