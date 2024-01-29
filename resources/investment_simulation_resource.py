f62b289196aeadbdadebf7e497883a82894eaa2e
Antonio Mourão <antonio.smsobrinho@gmail.com>
2024-01-29 12:42:13 -0300

    response schema

--- resources/investment_simulation_resource.py
+++ resources/investment_simulation_resource.py
@@ -4,6 +4,8 @@ from flasgger import swag_from
 from services.investment_simulation_service import InvestmentSimulationService
 from repositories.investment_simulation_repository import InvestmentSimulationRepository
 from validation_decorator.validation_simulation_investment import json_required, validate_investment_simulation_data
+from schemas.investment_simulation_response_schema import InvestmentSimulationResponseSchema
+
 class InvestmentSimulationResource(Resource):
 
     @swag_from('../swagger/investment_simulations/post.yml')
@@ -17,7 +19,8 @@ class InvestmentSimulationResource(Resource):
             InvestmentSimulationRepository()
         )
 
-        response_data = simulation_service.get_investment_values(data)
-        
-        return response_data, 201
+        investment_values = simulation_service.get_investment_values(data)
+        response_schema = InvestmentSimulationResponseSchema().dump(investment_values)
+
+        return response_schema, 201
     
