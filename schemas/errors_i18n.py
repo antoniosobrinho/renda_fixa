05e3055b32ec249bbd23c02910911db0c88645d0
Antonio Mourão <antonio.smsobrinho@gmail.com>
2024-01-29 10:19:45 -0300

    localize errors message on schema

---
+++ schemas/errors_i18n.py
@@ -0,0 +1,17 @@
+error_messages = {
+    'required': 'Este campo é obrigatório.',
+    'length': 'O comprimento deve ser maior que 0.',
+    'range': 'O valor deve ser maior ou igual a 0.',
+    'number_range': 'O número deve ser maior ou igual a 0.',
+    'validate': 'Erro de validação.',
+    'invalid': 'Valor inválido.',
+    'null': 'Este campo não pode ser nulo.',
+    'validator_failed': 'Erro de validação.',
+    'float': 'O valor deve ser um número decimal.',
+    'float_coerce': 'A conversão para número decimal falhou.',
+    'number_min': 'O número deve ser maior ou igual a 0.',
+    'number_max': 'O número deve ser menor ou igual a 0.',
+    'integer': 'O valor deve ser um número inteiro.',
+    'integer_coerce': 'A conversão para número inteiro falhou.',
+    'one_of': 'O valor deve ser "post-fixed" ou "pre-fixed".'
+}
--- schemas/investment_simulation_schema.py
+++ schemas/investment_simulation_schema.py
@@ -1,10 +1,11 @@
 from marshmallow import Schema, fields, validate
 from schemas.custom_fields import IntegerField, FloatField
+from schemas.errors_i18n import error_messages
 
 class InvestmentSimulationSchema(Schema):
-    name = fields.Str(required=True, validate=validate.Length(min=1))
-    months_invested = IntegerField(required=True, validate=validate.Range(min=1))
-    initial_value = FloatField(required=True, validate=validate.Range(min=0))
-    monthly_investment = FloatField(required=True, validate=validate.Range(min=0))
-    monthly_interest_rate = FloatField(required=True, validate=validate.Range(min=0))
-    compensation = fields.Str(required=True, validate=validate.OneOf(['post-fixed', 'pre-fixed']))
+    name = fields.Str(required=True, validate=validate.Length(min=1), error_messages=error_messages)
+    months_invested = IntegerField(required=True, validate=validate.Range(min=1), error_messages=error_messages)
+    initial_value = FloatField(required=True, validate=validate.Range(min=0), error_messages=error_messages)
+    monthly_investment = FloatField(required=True, validate=validate.Range(min=0), error_messages=error_messages)
+    monthly_interest_rate = FloatField(required=True, validate=validate.Range(min=0), error_messages=error_messages)
+    compensation = fields.Str(required=True, validate=validate.OneOf(['post-fixed', 'pre-fixed']), error_messages=error_messages)
